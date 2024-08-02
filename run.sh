5732d87a0e9670be1371cf0ebd502c4d34762443
myname1111 <animationstudiocp@gmail.com>
2024-08-02 15:39:38 +0700

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source conf.sh
-python3 odoo-bin -r $USERNAME -w $PASSWORD -d $DATABASE -addons-path=$ADDONS_PATH
+python3 odoo-bin -r $USERNAME -w $PASSWORD -d $DATABASE --addons-path=$ADDONS_PATH
